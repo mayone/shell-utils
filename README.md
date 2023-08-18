06a22f9300c45483c910ec63cea9398ddae90974
mayone <mayone321@gmail.com>
2023-08-18 16:18:29 +0800

    [Feature] Add png2icns

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Shell-Utilities
 
 ## utils
+
 - Scripts with common functions.
 - `index.sh`: Index file.
 
 ## Others
+
 - `gitconfig.sh`: Switch user of git config
 - `wsl_setup_dns.sh`: Setup DNS config on WSL
+- `png2icns.sh`: Convert PNG to macOS ICNS
+  - Reference: [PNG to ICNS Conversion Script and MacOS Automation](https://dustindavis.me/blog/png-to-icns-conversion-script-and-mac-os-automation)
 - `ffmpeg_wrapper.sh`: Wrapper for ffmpeg
-    - Reference: [protrolium/ffmpeg.md](https://gist.github.com/protrolium/e0dbd4bb0f1a396fcb55)
+  - Reference: [protrolium/ffmpeg.md](https://gist.github.com/protrolium/e0dbd4bb0f1a396fcb55)
---
+++ png2icns.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# Convert PNG to macOS ICNS.
+
+show_usage() {
+  echo "Usage:"
+  echo "  $@ <png_file>"
+  echo ""
+}
+
+main() {
+  if [ "$#" != 1 ]; then
+    show_usage "$0"
+    return
+  fi
+
+  SRC=$1
+  ICON_NAME="${SRC%.*}.icns"
+  echo "Converting $1 to $ICON_NAME..."
+
+  # Create an icon directory to work in
+  ICONS_DIR="tempicon.iconset"
+  mkdir $ICONS_DIR
+
+  # Create all other images sizes
+  sips -z 1024 1024 $1 --out "$ICONS_DIR/icon_512x512@2x.png"
+  sips -z 512  512  "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_512x512.png"
+  sips -z 512  512  "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_256x256@2x.png"
+  sips -z 256  256  "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_256x256x.png"
+  sips -z 256  256  "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_128x128@2x.png"
+  sips -z 128  128  "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_128x128.png"
+  sips -z 64   64   "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_64x64.png"
+  sips -z 32   32   "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_32x32.png"
+  sips -z 32   32   "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_16x16@2x.png"
+  sips -z 16   16   "$ICONS_DIR/icon_512x512@2x.png" --out "$ICONS_DIR/icon_16x16.png"
+
+  # Create the icns file
+  iconutil -c icns $ICONS_DIR
+
+  # Remove the temporary directory
+  rm -rf $ICONS_DIR
+
+  # Rename icns
+  mv tempicon.icns $ICON_NAME
+}
+
+main "$@"
