42da8768e856fc636065e0b3ee9adc21d41ac068
mayone <mayone321@gmail.com>
2024-08-25 18:03:23 +0800

    [Feature] Add m4a

--- yt_dlp_wrapper.sh
+++ yt_dlp_wrapper.sh
@@ -4,15 +4,19 @@
 
 # Commands
 MP4="mp4"
+M4A="m4a"
 
 CMDS="\
 ${MP4} \
+${M4A} \
 "
 
 main() {
   CMD="$1"
   if [ "$CMD" == "$MP4" ]; then
     mp4 "${@:2}"
+  elif [ "$CMD" == "$M4A" ]; then
+    m4a "${@:2}"
   else
     show_usage "$@"
   fi
@@ -41,6 +45,23 @@ mp4() {
   YT_URL="$1"
 
   yt-dlp -i "$YT_URL" -S vcodec:h264,res,acodec:m4a
+}
+
+#######################################
+# Download YouTube audio in m4a format.
+# Arguments:
+#   Link to the YouTube video.
+# Returns:
+#######################################
+m4a() {
+  if [[ "$#" != 1 ]]; then
+    echo "$M4A <YT_URL>        Download audio by the YouTube link."
+    return
+  fi
+
+  YT_URL="$1"
+
+  yt-dlp -i "$YT_URL" -f ba[ext=m4a]
 }
 
 main "$@"
